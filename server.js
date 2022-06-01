7cf14776ab4f54ade0bf22b3dc6714d28f1d9b2a
Ajay Paswan <codewithajaypaswan@gmail.com>
2022-06-01 08:45:36 +0530

    server changed

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use(express.static(staticPath));
 app.set("views", path.join(__dirname, "/views"));
 app.set("view engine", "ejs");
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5001;
 
 const connectDB = require("./config/db");
 connectDB();
